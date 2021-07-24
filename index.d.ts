e9654a052580165909524128f9edc32b8a68c8b5
Luke Edwards <luke.edwards05@gmail.com>
2021-07-24 12:38:58 -0700

    feat: add `canonical` plugin w/ rules

--- index.d.ts
+++ index.d.ts
@@ -5,13 +5,13 @@ export type Rules<T extends Dict = Dict> = {
 	[K in keyof T]: boolean | T[K];
 };
 
-export interface Context<T extends Dict = Dict> {
+export interface Context<T extends Dict = Rules> {
 	load<K extends keyof T>(title: K): T[K] | void;
 	report<K extends keyof T>(title: K, message: string): void;
 
 	assert<K extends keyof T>(title: K, check: boolean, message: string): void;
-	assert<K extends keyof T>(title: K, check: (options: T[K]) => boolean, message: string): void;
-	assert<K extends keyof T>(title: K, check: (options: T[K]) => Promise<boolean>, message: string): Promise<void>;
+	assert<K extends keyof T>(title: K, check: (options: Exclude<T[K],boolean>) => boolean, message: string): void;
+	assert<K extends keyof T>(title: K, check: (options: Exclude<T[K],boolean>) => Promise<boolean>, message: string): Promise<void>;
 }
 
 export type Plugin<R extends Rules = Rules> = (context: Context<R>, document: HTMLElement) => Promisable<void>;
