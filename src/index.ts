a5cfb2eb5b007395ab2fe6b35c5e07be39f1e3a9
Luke Edwards <luke.edwards05@gmail.com>
2021-07-24 21:02:12 -0700

    feat: hook up `run` main flow

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import { join, resolve } from 'path';
+import { request } from 'https';
+import { globalAgent } from 'http';
 import { parse } from 'node-html-parser';
+import { join, relative, resolve } from 'path';
 import { load } from './config';
 import * as fs from './utils/fs';
 
-import type { Argv, Config, Report, Messages } from 'seolint';
+import type { Argv, Context, Config } from 'seolint';
+import type { Report, Messages } from 'seolint';
 
-export async function lint(html: string, options?: Omit<Config, 'inputs'>) {
+export async function config(options: Argv = {}): Promise<Config> {
+	options.cwd = resolve(options.cwd || '.');
+
+	let value = await load(options.cwd) || {};
+	if (options.host) value.host = options.host;
+	if (options.input) value.inputs = options.input;
+
+	return value;
+}
+
+export async function lint(html: string, config: Omit<Config, 'inputs'>): Promise<Messages> {
 	let document = parse(html);
 	// normalize rules shape
 	let { rules, checks } = {} as any;
@@ -25,69 +38,91 @@ export async function lint(html: string, options?: Omit<Config, 'inputs'>) {
 	}
 }
 
-// export async function fetch(url: string, options?: any) {
-// 	//
-// }
+export async function file(path: string, config: Omit<Config, 'inputs'>): Promise<Report> {
+	let output: Report = {};
 
-export async function config(options: Argv = {}): Promise<Config> {
-	options.cwd = resolve(options.cwd || '.');
+	let stats = await fs.lstat(path);
 
-	let value = await load(options.cwd) || {};
-	if (options.input) value.inputs = options.input;
-	if (options.host) value.host = options.host;
+	if (stats.isFile()) {
+		if (!/\.html?$/.test(path)) return output;
+		let data = await fs.read(path, 'utf8');
 
-	// value.output = value.output || {};
+		return lint(data, config).then(msgs => {
+			output[path] = msgs;
+			return output;
+		});
+	}
 
-	// if (options.quiet) value.output.quiet = true;
-	// if (options.loglevel) value.output.loglevel = options.loglevel;
+	let arr = await fs.list(path);
 
-	return value;
+	await Promise.all(
+		arr.map(x => {
+			let nxt = join(path, x);
+			return file(nxt, config).then(rep => {
+				Object.assign(output, rep);
+			});
+		})
+	);
+
+	return output;
 }
 
-export async function file(): Promise<Messages> {
-	//
-}
+export function http(url: string, config: Omit<Config, 'inputs'>): Promise<Report> {
+	return new Promise((res, rej) => {
+		let content = '', output: Report = {};
+		let agent = /^http:\/\//.test(url) && globalAgent;
 
-export async function http(): Promise<Messages> {
-	//
+		let req = request(url, { agent }, r => {
+			let type = r.headers['content-type'] || '';
+			console.log('~> type:', type, r.statusCode);
+
+			if (!type.includes('text/html')) {
+				return rej('Invalid "Content-Type" header');
+			}
+
+			r.setEncoding('utf8');
+			r.on('data', d => { content += d });
+			r.on('end', async () => {
+				let msgs = await lint(content, config).catch(rej);
+				if (msgs) output[url] = msgs;
+				return res(output);
+			});
+		});
+
+		req.on('error', rej);
+	});
 }
 
 export async function run(config: Config, options: Argv): Promise<Report> {
-	// // let config = await load(options.cwd || '.');
-	// // let { rules, checks } = await config.load(options.cwd);
+	let output: Report = {};
+	let isHTTP=0, HTTP=/^https?:\/\//;
+	let cwd = options.cwd = resolve(options.cwd || '.');
+	let i=0, inputs = ([] as string[]).concat(config.inputs || []);
+	if (inputs.length === i) throw new Error('Missing inputs to analyze');
 
-	// let output: Report = {};
-	// let inputs = config.inputs || [];
-	// let i=0, CWD=resolve(options.cwd || '.');
-	// let isHTTP=0, isFILE=0, HTTP=/^https?:\/\//;
+	for (; i < inputs.length; i++) {
+		if (HTTP.test(inputs[i])) isHTTP++;
+		else inputs[i] = join(cwd, inputs[i]);
+	}
 
-	// if (!inputs.length) {
-	// 	throw new Error('Missing input(s) to analyze');
-	// }
+	if (isHTTP && isHTTP !== inputs.length) {
+		throw new Error('Inputs cannot contain both file-system and URL targets');
+	}
 
-	// for (; i < inputs.length; i++) {
-	// 	if (HTTP.test(inputs[i])) {
-	// 		isHTTP++;
-	// 	} else {
-	// 		inputs[i] = resolve(CWD, inputs[i]);
-	// 		isFILE++;
-	// 	}
+	let action = isHTTP ? http : file;
 
-	// 	if (isFILE && isHTTP) {
-	// 		throw new Error('Input array cannot contain file-system and URL targets');
-	// 	}
-	// }
+	await Promise.all(
+		inputs.map(input => {
+			return action(input, config).then(report => {
+				if (isHTTP) output = report;
+				else for (let key in report) {
+					output[relative(cwd, key)] = report[key];
+				}
+			}).catch(err => {
+				console.error('ERROR', input, err.stack);
+			});
+		})
+	);
 
-	// if (isHTTP) {
-	// 	await console.log('TODO');
-	// } else {
-	// 	await Promise.all(
-	// 		inputs.map(path => {
-
-	// 		})
-	// 	);
-	// }
-
-	// let stats = await fs.lstat(path);
-	// let single = stats.isFile();
+	return output;
 }
